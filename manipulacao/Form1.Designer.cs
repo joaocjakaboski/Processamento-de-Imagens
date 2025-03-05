f55bd477f48634a6b4089cbc922f1cc7e0067af6
joaocjakaboski <103127@aluno.uricer.edu.br>
2025-03-05 20:42:12 -0300

    Add project files.

---
+++ manipulacao/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace manipulacao {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.CaixaImagemImportada = new System.Windows.Forms.PictureBox();
+            this.botaoImportarImagem = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CaixaImagemImportada)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CaixaImagemImportada
+            // 
+            this.CaixaImagemImportada.Location = new System.Drawing.Point(12, 55);
+            this.CaixaImagemImportada.Name = "CaixaImagemImportada";
+            this.CaixaImagemImportada.Size = new System.Drawing.Size(200, 200);
+            this.CaixaImagemImportada.TabIndex = 0;
+            this.CaixaImagemImportada.TabStop = false;
+            // 
+            // botaoImportarImagem
+            // 
+            this.botaoImportarImagem.Location = new System.Drawing.Point(52, 12);
+            this.botaoImportarImagem.Name = "botaoImportarImagem";
+            this.botaoImportarImagem.Size = new System.Drawing.Size(116, 37);
+            this.botaoImportarImagem.TabIndex = 1;
+            this.botaoImportarImagem.Text = "Importar imagem";
+            this.botaoImportarImagem.UseVisualStyleBackColor = true;
+            this.botaoImportarImagem.Click += new System.EventHandler(this.botaoImportarImagem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.botaoImportarImagem);
+            this.Controls.Add(this.CaixaImagemImportada);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.CaixaImagemImportada)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CaixaImagemImportada;
+        private System.Windows.Forms.Button botaoImportarImagem;
+    }
+}
+
---
+++ manipulacao/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace manipulacao {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void botaoImportarImagem_Click(object sender, EventArgs e) {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Imagem|*.png;*.jpg;*.jpeg;*.bmp;*.tif";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                string imagePath = openFileDialog.FileName;
+
+                Bitmap imagem = new Bitmap(imagePath);
+                CaixaImagemImportada.Image = imagem;
+            }
+        }
+    }
+}
