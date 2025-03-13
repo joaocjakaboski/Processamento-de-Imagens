31d3c40c353cf8454b252d9e43f945c263c4c505
joaocjakaboski <103127@aluno.uricer.edu.br>
2025-03-12 21:58:26 -0300

    Implementada a função de mesclar imagens.

--- manipulacao/Form1.Designer.cs
+++ manipulacao/Form1.Designer.cs
@@ -23,47 +23,262 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.CaixaImagemImportada = new System.Windows.Forms.PictureBox();
-            this.botaoImportarImagem = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.CaixaImagemImportada)).BeginInit();
+            this.guia1CaixaImagemImportada = new System.Windows.Forms.PictureBox();
+            this.guia1BotaoImportarImagem = new System.Windows.Forms.Button();
+            this.guia1CaixaImagemEditada = new System.Windows.Forms.PictureBox();
+            this.guia1BotaoAdicionarBrilho = new System.Windows.Forms.Button();
+            this.guia1BotaoRemoverBrilho = new System.Windows.Forms.Button();
+            this.guia1EntradaValorBrilho = new System.Windows.Forms.NumericUpDown();
+            this.guia1 = new System.Windows.Forms.TabControl();
+            this.paginaGuia1 = new System.Windows.Forms.TabPage();
+            this.paginaGuia2 = new System.Windows.Forms.TabPage();
+            this.guia1Label1 = new System.Windows.Forms.Label();
+            this.guia2CaixaImagemImportada1 = new System.Windows.Forms.PictureBox();
+            this.guia2BotaoImportarImagem1 = new System.Windows.Forms.Button();
+            this.guia2CaixaImagemImportada2 = new System.Windows.Forms.PictureBox();
+            this.guia2BotaoImportarImagem2 = new System.Windows.Forms.Button();
+            this.guia2BotaoSoma = new System.Windows.Forms.Button();
+            this.guia2BotaoSubtracao = new System.Windows.Forms.Button();
+            this.guia2CaixaImagemEditada = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.guia1CaixaImagemImportada)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guia1CaixaImagemEditada)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guia1EntradaValorBrilho)).BeginInit();
+            this.guia1.SuspendLayout();
+            this.paginaGuia1.SuspendLayout();
+            this.paginaGuia2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guia2CaixaImagemImportada1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guia2CaixaImagemImportada2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guia2CaixaImagemEditada)).BeginInit();
             this.SuspendLayout();
             // 
-            // CaixaImagemImportada
+            // guia1CaixaImagemImportada
             // 
-            this.CaixaImagemImportada.Location = new System.Drawing.Point(12, 55);
-            this.CaixaImagemImportada.Name = "CaixaImagemImportada";
-            this.CaixaImagemImportada.Size = new System.Drawing.Size(200, 200);
-            this.CaixaImagemImportada.TabIndex = 0;
-            this.CaixaImagemImportada.TabStop = false;
+            this.guia1CaixaImagemImportada.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.guia1CaixaImagemImportada.Location = new System.Drawing.Point(21, 50);
+            this.guia1CaixaImagemImportada.Name = "guia1CaixaImagemImportada";
+            this.guia1CaixaImagemImportada.Size = new System.Drawing.Size(200, 200);
+            this.guia1CaixaImagemImportada.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guia1CaixaImagemImportada.TabIndex = 0;
+            this.guia1CaixaImagemImportada.TabStop = false;
             // 
-            // botaoImportarImagem
+            // guia1BotaoImportarImagem
             // 
-            this.botaoImportarImagem.Location = new System.Drawing.Point(52, 12);
-            this.botaoImportarImagem.Name = "botaoImportarImagem";
-            this.botaoImportarImagem.Size = new System.Drawing.Size(116, 37);
-            this.botaoImportarImagem.TabIndex = 1;
-            this.botaoImportarImagem.Text = "Importar imagem";
-            this.botaoImportarImagem.UseVisualStyleBackColor = true;
-            this.botaoImportarImagem.Click += new System.EventHandler(this.botaoImportarImagem_Click);
+            this.guia1BotaoImportarImagem.Location = new System.Drawing.Point(61, 7);
+            this.guia1BotaoImportarImagem.Name = "guia1BotaoImportarImagem";
+            this.guia1BotaoImportarImagem.Size = new System.Drawing.Size(116, 37);
+            this.guia1BotaoImportarImagem.TabIndex = 1;
+            this.guia1BotaoImportarImagem.Text = "Importar imagem";
+            this.guia1BotaoImportarImagem.UseVisualStyleBackColor = true;
+            this.guia1BotaoImportarImagem.Click += new System.EventHandler(this.botaoImportarImagem_Click);
+            // 
+            // guia1CaixaImagemEditada
+            // 
+            this.guia1CaixaImagemEditada.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.guia1CaixaImagemEditada.Location = new System.Drawing.Point(516, 50);
+            this.guia1CaixaImagemEditada.Name = "guia1CaixaImagemEditada";
+            this.guia1CaixaImagemEditada.Size = new System.Drawing.Size(200, 200);
+            this.guia1CaixaImagemEditada.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guia1CaixaImagemEditada.TabIndex = 2;
+            this.guia1CaixaImagemEditada.TabStop = false;
+            // 
+            // guia1BotaoAdicionarBrilho
+            // 
+            this.guia1BotaoAdicionarBrilho.Location = new System.Drawing.Point(13, 279);
+            this.guia1BotaoAdicionarBrilho.Name = "guia1BotaoAdicionarBrilho";
+            this.guia1BotaoAdicionarBrilho.Size = new System.Drawing.Size(101, 35);
+            this.guia1BotaoAdicionarBrilho.TabIndex = 3;
+            this.guia1BotaoAdicionarBrilho.Text = "Adicionar Brilho";
+            this.guia1BotaoAdicionarBrilho.UseVisualStyleBackColor = true;
+            this.guia1BotaoAdicionarBrilho.Click += new System.EventHandler(this.botaoAdicionarBrilho_Click);
+            // 
+            // guia1BotaoRemoverBrilho
+            // 
+            this.guia1BotaoRemoverBrilho.Location = new System.Drawing.Point(120, 279);
+            this.guia1BotaoRemoverBrilho.Name = "guia1BotaoRemoverBrilho";
+            this.guia1BotaoRemoverBrilho.Size = new System.Drawing.Size(101, 35);
+            this.guia1BotaoRemoverBrilho.TabIndex = 4;
+            this.guia1BotaoRemoverBrilho.Text = "Remover";
+            this.guia1BotaoRemoverBrilho.UseVisualStyleBackColor = true;
+            this.guia1BotaoRemoverBrilho.Click += new System.EventHandler(this.botaoRemoverBrilho_Click);
+            // 
+            // guia1EntradaValorBrilho
+            // 
+            this.guia1EntradaValorBrilho.Location = new System.Drawing.Point(58, 253);
+            this.guia1EntradaValorBrilho.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.guia1EntradaValorBrilho.Name = "guia1EntradaValorBrilho";
+            this.guia1EntradaValorBrilho.Size = new System.Drawing.Size(120, 20);
+            this.guia1EntradaValorBrilho.TabIndex = 6;
+            // 
+            // guia1
+            // 
+            this.guia1.Controls.Add(this.paginaGuia1);
+            this.guia1.Controls.Add(this.paginaGuia2);
+            this.guia1.Location = new System.Drawing.Point(1, 2);
+            this.guia1.Name = "guia1";
+            this.guia1.SelectedIndex = 0;
+            this.guia1.Size = new System.Drawing.Size(935, 672);
+            this.guia1.TabIndex = 9;
+            // 
+            // paginaGuia1
+            // 
+            this.paginaGuia1.Controls.Add(this.guia1Label1);
+            this.paginaGuia1.Controls.Add(this.guia1CaixaImagemImportada);
+            this.paginaGuia1.Controls.Add(this.guia1BotaoImportarImagem);
+            this.paginaGuia1.Controls.Add(this.guia1CaixaImagemEditada);
+            this.paginaGuia1.Controls.Add(this.guia1EntradaValorBrilho);
+            this.paginaGuia1.Controls.Add(this.guia1BotaoAdicionarBrilho);
+            this.paginaGuia1.Controls.Add(this.guia1BotaoRemoverBrilho);
+            this.paginaGuia1.Location = new System.Drawing.Point(4, 22);
+            this.paginaGuia1.Name = "paginaGuia1";
+            this.paginaGuia1.Padding = new System.Windows.Forms.Padding(3);
+            this.paginaGuia1.Size = new System.Drawing.Size(927, 646);
+            this.paginaGuia1.TabIndex = 0;
+            this.paginaGuia1.Text = "Brilho";
+            this.paginaGuia1.UseVisualStyleBackColor = true;
+            // 
+            // paginaGuia2
+            // 
+            this.paginaGuia2.Controls.Add(this.guia2CaixaImagemEditada);
+            this.paginaGuia2.Controls.Add(this.guia2BotaoSubtracao);
+            this.paginaGuia2.Controls.Add(this.guia2BotaoSoma);
+            this.paginaGuia2.Controls.Add(this.guia2CaixaImagemImportada2);
+            this.paginaGuia2.Controls.Add(this.guia2BotaoImportarImagem2);
+            this.paginaGuia2.Controls.Add(this.guia2CaixaImagemImportada1);
+            this.paginaGuia2.Controls.Add(this.guia2BotaoImportarImagem1);
+            this.paginaGuia2.Location = new System.Drawing.Point(4, 22);
+            this.paginaGuia2.Name = "paginaGuia2";
+            this.paginaGuia2.Padding = new System.Windows.Forms.Padding(3);
+            this.paginaGuia2.Size = new System.Drawing.Size(927, 646);
+            this.paginaGuia2.TabIndex = 1;
+            this.paginaGuia2.Text = "Mesclar Imagens";
+            this.paginaGuia2.UseVisualStyleBackColor = true;
+            // 
+            // guia1Label1
+            // 
+            this.guia1Label1.AutoSize = true;
+            this.guia1Label1.Location = new System.Drawing.Point(513, 31);
+            this.guia1Label1.Name = "guia1Label1";
+            this.guia1Label1.Size = new System.Drawing.Size(88, 13);
+            this.guia1Label1.TabIndex = 7;
+            this.guia1Label1.Text = "Imagem alterada:";
+            // 
+            // guia2CaixaImagemImportada1
+            // 
+            this.guia2CaixaImagemImportada1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.guia2CaixaImagemImportada1.Location = new System.Drawing.Point(7, 48);
+            this.guia2CaixaImagemImportada1.Name = "guia2CaixaImagemImportada1";
+            this.guia2CaixaImagemImportada1.Size = new System.Drawing.Size(200, 200);
+            this.guia2CaixaImagemImportada1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guia2CaixaImagemImportada1.TabIndex = 2;
+            this.guia2CaixaImagemImportada1.TabStop = false;
+            // 
+            // guia2BotaoImportarImagem1
+            // 
+            this.guia2BotaoImportarImagem1.Location = new System.Drawing.Point(47, 5);
+            this.guia2BotaoImportarImagem1.Name = "guia2BotaoImportarImagem1";
+            this.guia2BotaoImportarImagem1.Size = new System.Drawing.Size(116, 37);
+            this.guia2BotaoImportarImagem1.TabIndex = 3;
+            this.guia2BotaoImportarImagem1.Text = "Importar imagem";
+            this.guia2BotaoImportarImagem1.UseVisualStyleBackColor = true;
+            this.guia2BotaoImportarImagem1.Click += new System.EventHandler(this.guia2BotaoImportarImagem1_Click);
+            // 
+            // guia2CaixaImagemImportada2
+            // 
+            this.guia2CaixaImagemImportada2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.guia2CaixaImagemImportada2.Location = new System.Drawing.Point(236, 48);
+            this.guia2CaixaImagemImportada2.Name = "guia2CaixaImagemImportada2";
+            this.guia2CaixaImagemImportada2.Size = new System.Drawing.Size(200, 200);
+            this.guia2CaixaImagemImportada2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guia2CaixaImagemImportada2.TabIndex = 4;
+            this.guia2CaixaImagemImportada2.TabStop = false;
+            // 
+            // guia2BotaoImportarImagem2
+            // 
+            this.guia2BotaoImportarImagem2.Location = new System.Drawing.Point(276, 5);
+            this.guia2BotaoImportarImagem2.Name = "guia2BotaoImportarImagem2";
+            this.guia2BotaoImportarImagem2.Size = new System.Drawing.Size(116, 37);
+            this.guia2BotaoImportarImagem2.TabIndex = 5;
+            this.guia2BotaoImportarImagem2.Text = "Importar imagem";
+            this.guia2BotaoImportarImagem2.UseVisualStyleBackColor = true;
+            this.guia2BotaoImportarImagem2.Click += new System.EventHandler(this.guia2BotaoImportarImagem2_Click);
+            // 
+            // guia2BotaoSoma
+            // 
+            this.guia2BotaoSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guia2BotaoSoma.Location = new System.Drawing.Point(454, 106);
+            this.guia2BotaoSoma.Name = "guia2BotaoSoma";
+            this.guia2BotaoSoma.Size = new System.Drawing.Size(30, 30);
+            this.guia2BotaoSoma.TabIndex = 6;
+            this.guia2BotaoSoma.Text = "+";
+            this.guia2BotaoSoma.UseVisualStyleBackColor = true;
+            this.guia2BotaoSoma.Click += new System.EventHandler(this.guia2BotaoSoma_Click);
+            // 
+            // guia2BotaoSubtracao
+            // 
+            this.guia2BotaoSubtracao.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guia2BotaoSubtracao.Location = new System.Drawing.Point(454, 142);
+            this.guia2BotaoSubtracao.Name = "guia2BotaoSubtracao";
+            this.guia2BotaoSubtracao.Size = new System.Drawing.Size(30, 30);
+            this.guia2BotaoSubtracao.TabIndex = 7;
+            this.guia2BotaoSubtracao.Text = "-";
+            this.guia2BotaoSubtracao.UseVisualStyleBackColor = true;
+            this.guia2BotaoSubtracao.Click += new System.EventHandler(this.guia2BotaoSubtracao_Click);
+            // 
+            // guia2CaixaImagemEditada
+            // 
+            this.guia2CaixaImagemEditada.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.guia2CaixaImagemEditada.Location = new System.Drawing.Point(503, 48);
+            this.guia2CaixaImagemEditada.Name = "guia2CaixaImagemEditada";
+            this.guia2CaixaImagemEditada.Size = new System.Drawing.Size(200, 200);
+            this.guia2CaixaImagemEditada.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guia2CaixaImagemEditada.TabIndex = 8;
+            this.guia2CaixaImagemEditada.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.botaoImportarImagem);
-            this.Controls.Add(this.CaixaImagemImportada);
+            this.ClientSize = new System.Drawing.Size(937, 672);
+            this.Controls.Add(this.guia1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.CaixaImagemImportada)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guia1CaixaImagemImportada)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guia1CaixaImagemEditada)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guia1EntradaValorBrilho)).EndInit();
+            this.guia1.ResumeLayout(false);
+            this.paginaGuia1.ResumeLayout(false);
+            this.paginaGuia1.PerformLayout();
+            this.paginaGuia2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guia2CaixaImagemImportada1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guia2CaixaImagemImportada2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guia2CaixaImagemEditada)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox CaixaImagemImportada;
-        private System.Windows.Forms.Button botaoImportarImagem;
+        private System.Windows.Forms.PictureBox guia1CaixaImagemImportada;
+        private System.Windows.Forms.Button guia1BotaoImportarImagem;
+        private System.Windows.Forms.PictureBox guia1CaixaImagemEditada;
+        private System.Windows.Forms.Button guia1BotaoAdicionarBrilho;
+        private System.Windows.Forms.Button guia1BotaoRemoverBrilho;
+        private System.Windows.Forms.NumericUpDown guia1EntradaValorBrilho;
+        private System.Windows.Forms.TabControl guia1;
+        private System.Windows.Forms.TabPage paginaGuia1;
+        private System.Windows.Forms.TabPage paginaGuia2;
+        private System.Windows.Forms.Label guia1Label1;
+        private System.Windows.Forms.PictureBox guia2CaixaImagemImportada1;
+        private System.Windows.Forms.Button guia2BotaoImportarImagem1;
+        private System.Windows.Forms.PictureBox guia2CaixaImagemImportada2;
+        private System.Windows.Forms.Button guia2BotaoImportarImagem2;
+        private System.Windows.Forms.Button guia2BotaoSoma;
+        private System.Windows.Forms.Button guia2BotaoSubtracao;
+        private System.Windows.Forms.PictureBox guia2CaixaImagemEditada;
     }
 }
 
--- manipulacao/Form1.cs
+++ manipulacao/Form1.cs
@@ -10,19 +10,218 @@ using System.Windows.Forms;
 
 namespace manipulacao {
     public partial class Form1 : Form {
+
+        Bitmap img1;
+        Bitmap img2;
+        byte[,] vImg1Gray;
+
+        byte[,] vImg1R;
+        byte[,] vImg1G;
+        byte[,] vImg1B;
+        byte[,] vImg1A;
+
+        bool bLoadImgOK = false;
+
         public Form1() {
             InitializeComponent();
         }
 
         private void botaoImportarImagem_Click(object sender, EventArgs e) {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Imagem|*.png;*.jpg;*.jpeg;*.bmp;*.tif";
+            Boolean imagemCarregada = carregarImagem();
 
-            if (openFileDialog.ShowDialog() == DialogResult.OK) {
-                string imagePath = openFileDialog.FileName;
+            if (imagemCarregada == true) {
+                guia1CaixaImagemImportada.Image = img1;
+            }                     
+        }
 
-                Bitmap imagem = new Bitmap(imagePath);
-                CaixaImagemImportada.Image = imagem;
+        private void botaoAdicionarBrilho_Click(object sender, EventArgs e) {
+
+            Bitmap imagem = (Bitmap)guia1CaixaImagemImportada.Image;
+
+            // Se a imagem carregou perfeitamente...
+            if (bLoadImgOK == true) {
+                vImg1Gray = new byte[imagem.Width, imagem.Height];
+
+                // Percorre todos os pixels da imagem...
+                for (int i = 0; i < imagem.Width; i++) {
+                    for (int j = 0; j < imagem.Height; j++) {
+                        Color pixel = imagem.GetPixel(i, j);
+
+                        int valorR = pixel.R + (int)guia1EntradaValorBrilho.Value;
+                        int valorG = pixel.G + (int)guia1EntradaValorBrilho.Value;
+                        int valorB = pixel.B + (int)guia1EntradaValorBrilho.Value;
+
+                        if (valorR > 255) {
+                            valorR = 255;
+                        }
+
+                        if (valorG > 255) {
+                            valorG = 255;
+                        }
+
+                        if (valorB > 255) {
+                            valorB = 255;
+                        }
+
+                        Color cor = Color.FromArgb(255, valorR, valorG, valorB);
+
+                        img2.SetPixel(i, j, cor);
+                    }
+                }
+                guia1CaixaImagemEditada.Image = img2;
+            }
+            
+        }
+
+        private void botaoRemoverBrilho_Click(object sender, EventArgs e) {
+
+            Bitmap imagem = (Bitmap)guia1CaixaImagemImportada.Image;
+
+            // Se a imagem carregou perfeitamente...
+            if (bLoadImgOK == true) {
+                vImg1Gray = new byte[imagem.Width, imagem.Height];
+
+                // Percorre todos os pixels da imagem...
+                for (int i = 0; i < imagem.Width; i++) {
+                    for (int j = 0; j < imagem.Height; j++) {
+                        Color pixel = imagem.GetPixel(i, j);
+
+                        int valorR = pixel.R - (int)guia1EntradaValorBrilho.Value;
+                        int valorG = pixel.G - (int)guia1EntradaValorBrilho.Value;
+                        int valorB = pixel.B - (int)guia1EntradaValorBrilho.Value;
+
+                        if (valorR < 0) {
+                            valorR = 0;
+                        }
+
+                        if (valorG < 0) {
+                            valorG = 0;
+                        }
+
+                        if (valorB < 0) {
+                            valorB = 0;
+                        }
+
+                        Color cor = Color.FromArgb(255, valorR, valorG, valorB);
+
+                        img2.SetPixel(i, j, cor);
+                    }
+                }
+                guia1CaixaImagemEditada.Image = img2;
+            }
+        }
+
+        private void guia2BotaoImportarImagem1_Click(object sender, EventArgs e) {
+            Boolean imagemCarregada = carregarImagem();
+
+            if (imagemCarregada == true) {
+                guia2CaixaImagemImportada1.Image = img1;
+            }
+        }
+
+        private void guia2BotaoImportarImagem2_Click(object sender, EventArgs e) {
+            Boolean imagemCarregada = carregarImagem();
+
+            if (imagemCarregada == true) {
+                guia2CaixaImagemImportada2.Image = img1;
+            }
+        }
+
+        private Boolean carregarImagem() {
+            // Configurações iniciais da OpenFileDialogBox
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            var filePath = string.Empty;
+            openFileDialog1.InitialDirectory = "C:\\Users\\João\\Documents\\Faculdade\\5_semestre\\Processamento de Imagens\\Material Matlab\\Matlab";
+            openFileDialog1.Filter = "TIFF image (*.tif)|*.tif|JPG image (*.jpg)|*.jpg|BMP image (*.bmp)|*.bmp|PNG image (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            // Se um arquivo foi localizado com sucesso...
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                // Armnazena o path do arquivo de imagem
+                filePath = openFileDialog1.FileName;
+
+                try {
+                    img1 = new Bitmap(filePath);
+                    img2 = new Bitmap(img1.Width, img1.Height);
+                    bLoadImgOK = true;
+                } catch (Exception ex) {
+                    MessageBox.Show(ex.Message, "Erro ao abrir imagem...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    bLoadImgOK = false;
+                }
+            }
+            return bLoadImgOK;
+        }
+
+        private void guia2BotaoSoma_Click(object sender, EventArgs e) {
+            Bitmap imagem1 = (Bitmap)guia2CaixaImagemImportada1.Image;
+            Bitmap imagem2 = (Bitmap)guia2CaixaImagemImportada2.Image;
+
+            if (imagem1.Width == imagem2.Width && imagem1.Height == imagem2.Height) {
+                // Percorre todos os pixels da imagem...
+                for (int i = 0; i < imagem1.Width; i++) {
+                    for (int j = 0; j < imagem1.Height; j++) {
+                        Color pixel = imagem1.GetPixel(i, j);
+                        Color pixel2 = imagem2.GetPixel(i, j);
+
+                        int valorR = pixel.R + pixel2.R;
+                        int valorG = pixel.G + pixel2.G;
+                        int valorB = pixel.B + pixel2.B;
+
+                        if (valorR > 255) {
+                            valorR = 255;
+                        }
+
+                        if (valorG > 255) {
+                            valorG = 255;
+                        }
+
+                        if (valorB > 255) {
+                            valorB = 255;
+                        }
+
+                        Color cor = Color.FromArgb(255, valorR, valorG, valorB);
+
+                        img2.SetPixel(i, j, cor);
+                    }
+                }
+                guia2CaixaImagemEditada.Image = img2;
+            }
+        }
+
+        private void guia2BotaoSubtracao_Click(object sender, EventArgs e) {
+            Bitmap imagem1 = (Bitmap)guia2CaixaImagemImportada1.Image;
+            Bitmap imagem2 = (Bitmap)guia2CaixaImagemImportada2.Image;
+
+            if (imagem1.Width == imagem2.Width && imagem1.Height == imagem2.Height) {
+                // Percorre todos os pixels da imagem...
+                for (int i = 0; i < imagem1.Width; i++) {
+                    for (int j = 0; j < imagem1.Height; j++) {
+                        Color pixel = imagem1.GetPixel(i, j);
+                        Color pixel2 = imagem2.GetPixel(i, j);
+
+                        int valorR = pixel.R - pixel2.R;
+                        int valorG = pixel.G - pixel2.G;
+                        int valorB = pixel.B - pixel2.B;
+
+                        if (valorR < 0) {
+                            valorR = 0;
+                        }
+
+                        if (valorG < 0) {
+                            valorG = 0;
+                        }
+
+                        if (valorB < 0) {
+                            valorB = 0;
+                        }
+
+                        Color cor = Color.FromArgb(255, valorR, valorG, valorB);
+
+                        img2.SetPixel(i, j, cor);
+                    }
+                }
+                guia2CaixaImagemEditada.Image = img2;
             }
         }
     }
